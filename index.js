c342986e941d9753dab4fbc0a4f47a05cbc38990
AshokYadav <yashoka51@gmail.com>
2023-10-26 21:20:22 +0530

    this contain get data from axios

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let form=document.getElementById('addForm');
 
+let ul=document.getElementById('items');
+
 form.addEventListener('submit',addFrom);
 
 
@@ -15,8 +17,33 @@ function addFrom(e){
         name:name.value,
         number:number.value
     }
-    
+     
+    showOnScreen(obj);
     axios.post('https://crudcrud.com/api/44314eabb8ec4ec5b3c5c2961b65a81f/appointmentDate',obj)
     .then((response)=>console.log(response))
     .catch(error=>console.log(error))
+
+    email.value='';
+    name.value='';
+    number.value='';
+}
+
+window.addEventListener('DOMContentLoaded',()=>{
+    axios.get('https://crudcrud.com/api/44314eabb8ec4ec5b3c5c2961b65a81f/appointmentDate')
+    .then((response)=>{
+        for(let i=0;i<response.data.length;i++){
+            showOnScreen(response.data[i]);
+        }
+    })
+    .catch(error=>console.log(error))
+})
+
+function showOnScreen(obj){
+    let li=document.createElement('li');
+    li.classList='list-group-item';
+    let str=`${obj.email} - ${obj.name} - ${obj.number}`;
+    li.appendChild(document.createTextNode(str));
+
+    ul.appendChild(li);
+
 }
